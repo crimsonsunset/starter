9b2c56ff87f8dcd051add418babec9e5d1a48e42
Joseph Sangiorgio <jsangio1@gmail.com>
2018-04-11 20:11:10 -0400

    feat: start of layout

--- app/components/App.js
+++ app/components/App.js
@@ -15,20 +15,56 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-light">
-          <ul className="nav navbar-nav">
+      <div className={'app'}>
 
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/playground">Playground</Link></li>
-            <li><Link to="/not-found">Not Found</Link></li>
+        <nav class="navbar is-dark">
+          <div class="navbar-brand">
+            <a class="navbar-item" href="https://bulma.io">
+              <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox"
+                   width="112" height="28"/>
+            </a>
+          </div>
 
-          </ul>
+          <div id="navbarExampleTransparentExample" class="navbar-menu">
+            <div class="navbar-start">
+              <a class="navbar-item" href="https://bulma.io/">
+                Home
+              </a>
+
+            </div>
+
+            {/*<div class="navbar-end">*/}
+              {/*<div class="navbar-item">*/}
+                {/*<div class="field is-grouped">*/}
+                  {/*<p class="control">*/}
+                    {/*<a class="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet"*/}
+                       {/*data-social-target="http://localhost:4000" target="_blank"*/}
+                       {/*href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=http://localhost:4000&amp;via=jgthms">*/}
+                      {/*<span>*/}
+                {/*Tweet*/}
+              {/*</span>*/}
+                    {/*</a>*/}
+                  {/*</p>*/}
+                {/*</div>*/}
+              {/*</div>*/}
+            {/*</div>*/}
+
+          </div>
         </nav>
 
-        {/*todo: protected routes and redirects */}
-        {/*https://www.sitepoint.com/react-router-v4-complete-guide/*/}
         <Routes/>
+
+        <footer class="footer">
+          <div class="container">
+            <div class="content has-text-centered">
+              <p>
+                <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
+                <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+                is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+              </p>
+            </div>
+          </div>
+        </footer>
 
       </div>
 
--- app/components/views/HomeView.js
+++ app/components/views/HomeView.js
@@ -1,19 +1,101 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types';
 // import routes from '../config/routes'
-// import {map} from 'lodash'
+import mainLogo from'../../assets/svg/search.svg';
 
 class HomeView extends Component {
   constructor(props, context) {
     super(props, context);
   }
 
-  componentDidMount(){
+  componentDidMount() {
     console.log(this);
   }
 
-  render(){
-    return <h2>HomeView</h2>
+  render() {
+    return (<section>
+        {/*<h1>Homeview</h1>*/}
+
+        <nav className="panel">
+          <p className="panel-heading">
+            Job Search
+          </p>
+          <div className="panel-block">
+            <p className="control has-icons-left">
+              <input className="input is-medium" type="text" placeholder="search"/>
+              {/*<span className="icon is-small is-left">*/}
+        {/*<i className="fas fa-search" aria-hidden="true"></i>*/}
+      {/*</span>*/}
+
+              <span class="icon is-small is-left">
+  <object data={mainLogo} type="image/svg+xml">
+  <img src="yourfallback.jpg"/>
+</object>
+</span>
+
+            </p>
+          </div>
+          {/*<p className="panel-tabs">*/}
+          {/*<a className="is-active">all</a>*/}
+          {/*<a>public</a>*/}
+          {/*<a>private</a>*/}
+          {/*<a>sources</a>*/}
+          {/*<a>forks</a>*/}
+          {/*</p>*/}
+          {/*<a className="panel-block is-active">*/}
+          {/*<span className="panel-icon">*/}
+          {/*<i className="fas fa-book" aria-hidden="true"></i>*/}
+          {/*</span>*/}
+          {/*bulma*/}
+          {/*</a>*/}
+          {/*<a className="panel-block">*/}
+          {/*<span className="panel-icon">*/}
+          {/*<i className="fas fa-book" aria-hidden="true"></i>*/}
+          {/*</span>*/}
+          {/*marksheet*/}
+          {/*</a>*/}
+          {/*<a className="panel-block">*/}
+          {/*<span className="panel-icon">*/}
+          {/*<i className="fas fa-book" aria-hidden="true"></i>*/}
+          {/*</span>*/}
+          {/*minireset.css*/}
+          {/*</a>*/}
+          {/*<a className="panel-block">*/}
+          {/*<span className="panel-icon">*/}
+          {/*<i className="fas fa-book" aria-hidden="true"></i>*/}
+          {/*</span>*/}
+          {/*jgthms.github.io*/}
+          {/*</a>*/}
+          {/*<a className="panel-block">*/}
+          {/*<span className="panel-icon">*/}
+          {/*<i className="fas fa-code-fork" aria-hidden="true"></i>*/}
+          {/*</span>*/}
+          {/*daniellowtw/infboard*/}
+          {/*</a>*/}
+          {/*<a className="panel-block">*/}
+          {/*<span className="panel-icon">*/}
+          {/*<i className="fas fa-code-fork" aria-hidden="true"></i>*/}
+          {/*</span>*/}
+          {/*mojs*/}
+          {/*</a>*/}
+          {/*<label className="panel-block">*/}
+          {/*<input type="checkbox"/>*/}
+          {/*remember me*/}
+          {/*</label>*/}
+          {/*<div className="panel-block">*/}
+          {/*<button className="button is-link is-outlined is-fullwidth">*/}
+          {/*reset all filters*/}
+          {/*</button>*/}
+          {/*</div>*/}
+
+
+        </nav>
+
+
+      </section>
+    )
+
+
   }
 }
 
